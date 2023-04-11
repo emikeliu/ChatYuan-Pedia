b434ccd91f4a021de4e6e7823322c9064631a315
Mike Liu <imikeliu233@outlook.com>
2023-04-11 16:11:55 +0800

    FASTAPI

--- NewYuan.py
+++ NewYuan.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import ORJSONResponse
 from pydantic import BaseModel
 from typing import List
 import json
